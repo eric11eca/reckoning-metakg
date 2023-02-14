7300ff8776f8546dc7c1951d9d07748f82ad415c
eric11eca <chenz16@rose-hulman.edu>
2023-02-14 02:34:41 +0100

    accomadate cloud build file

--- build_cuda11_m1.sh
+++ build_cuda11_m1.sh
@@ -1,8 +1,8 @@
-NUM=8
+NUM=12
 
 CURRENT=${NUM}
 IMAGE_NAME=meta_kg
-DOCKERFILE_NAME=DockerfileV2
+DOCKERFILE_NAME=docker/update_train/Dockerfile
 
 GIT_HASH=`git log --format="%h" -n 1`
 IMAGE=$IMAGE_NAME_$USER-$GIT_HASH
@@ -12,9 +12,6 @@ echo "Building $IM_NAME"
 docker buildx build --build-arg DUMMY='cloud' --platform linux/amd64 --load -f $DOCKERFILE_NAME -t $IM_NAME --cache-from type=local,src=../../.docker_cache --cache-to type=local,mode=max,dest=../../.docker_cache .
 
 echo "Pushing $IM_NAME to Harbor"
-# docker tag $IM_NAME ic-registry.epfl.ch/nlp/zechen/$IM_NAME
-# docker push ic-registry.epfl.ch/nlp/zechen/$IM_NAME
-
 docker tag $IM_NAME eric11eca/$IM_NAME
 docker push eric11eca/$IM_NAME
 
