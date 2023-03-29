fc29b777aa7241b91436fa1ca3e97e0f15b7cab9
eric11eca <chenz16@rose-hulman.edu>
2023-03-29 15:22:22 +0200

    add cloud computation runai's auto workflow

---
+++ launch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+CLUSTER_USER=zechen
+CLUSTER_USER_ID=254670
+CLUSTER_GROUP_NAME=NLP-StaffU
+CLUSTER_GROUP_ID=11131
+
+MY_IMAGE="eric11eca/meta_kg_7"
+
+arg_job_prefix="meta-lora"
+arg_job_suffix="proof2h"
+arg_job_name="$arg_job_prefix-$arg_job_suffix"
+
+command=$1
+
+if [ "$command" == "run" ]; then
+	echo "Job [$arg_job_name]"
+
+	runai submit $arg_job_name \
+		-i $MY_IMAGE \
+		--gpu 1 \
+		# --node-type G10
+		
+		# -e CLUSTER_USER=$CLUSTER_USER \
+		# -e CLUSTER_USER_ID=$CLUSTER_USER_ID \
+		# -e CLUSTER_GROUP_NAME=$CLUSTER_GROUP_NAME \
+		# -e CLUSTER_GROUP_ID=$CLUSTER_GROUP_ID \
+		
+		# --pvc runai-nlp-zechen-nlpdata1:/nlpdata1 \
+		# --large-shm \
+		# --service-type=nodeport \
+		# --port 30012:22
+	exit 0
+fi
+
+if [ "$command" == "log" ]; then
+	runai logs $arg_job_name -f
+	exit 0
+fi
+
+if [ "$command" == "stat" ]; then
+	runai describe job $arg_job_name 
+	exit 0
+fi
+
+if [ "$command" == "del" ]; then
+	runai delete job $arg_job_name
+	exit 0
+fi
+
+
+
+if [ $? -eq 0 ]; then
+	runai describe job $arg_job_name
+fi
