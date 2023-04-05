1c08405571eb8acc4b95bb920f13d3309a5e7e38
eric11eca <chenz16@rose-hulman.edu>
2023-04-06 00:53:35 +0200

    cloud training submit bash

--- launch.sh
+++ launch.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-CLUSTER_USER=zechen
-CLUSTER_USER_ID=254670
-CLUSTER_GROUP_NAME=NLP-StaffU
-CLUSTER_GROUP_ID=11131
-
-MY_IMAGE="eric11eca/meta_kg_step2_5"
+MY_IMAGE="eric11eca/meta_kg_7"
 
 arg_job_prefix="meta-kg"
-arg_job_suffix="proof5-s2"
+arg_job_suffix="clutrr2"
 arg_job_name="$arg_job_prefix-$arg_job_suffix"
 
 command=$1
@@ -19,17 +14,23 @@ if [ "$command" == "run" ]; then
 	runai submit $arg_job_name \
 		-i $MY_IMAGE \
 		--gpu 1 \
-		--node-type G10
-		
-		# -e CLUSTER_USER=$CLUSTER_USER \
-		# -e CLUSTER_USER_ID=$CLUSTER_USER_ID \
-		# -e CLUSTER_GROUP_NAME=$CLUSTER_GROUP_NAME \
-		# -e CLUSTER_GROUP_ID=$CLUSTER_GROUP_ID \
-		
+		--node-type G10 \
 		# --pvc runai-nlp-zechen-nlpdata1:/nlpdata1 \
-		# --large-shm \
-		# --service-type=nodeport \
-		# --port 30012:22
+		# --large-shm
+	exit 0
+fi
+
+if [ "$command" == "run_bash" ]; then
+	echo "Job [$arg_job_name]"
+
+	runai submit $arg_job_name \
+		-i $MY_IMAGE \
+		--gpu 4 \
+		--pvc runai-nlp-zechen-nlpdata1:/nlpdata1 \
+		--large-shm \
+		--interactive \
+		--attach \
+		--node-type G10
 	exit 0
 fi
 
