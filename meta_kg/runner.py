0c09a3b7663bbb323a166e2283afda122b3f6852
Zeming Chen <zeming.chen@epfl.ch>
2023-04-28 19:10:27 +0200

    proper handling of data and model checkpoint using wandb

--- meta_kg/runner.py
+++ meta_kg/runner.py
@@ -1,10 +1,5 @@
 import os
 import logging
-# import hydra
-
-# from pprint import pprint
-# from typing import Optional
-# from omegaconf import DictConfig, OmegaConf
 
 from meta_kg.train import setup_trainer
 from meta_kg.utils.wandb_utils import setup_wandb
@@ -34,6 +29,8 @@ def run(args):
     util_logger.info('Setting up configuration for model runner...')
     setup_wandb(args)
 
+    print(args.checkpoint)
+
     if args.baseline:
         args.inner_mode = 'baseline'
 
@@ -41,9 +38,9 @@ def run(args):
     trainer = setup_trainer(args)
 
     if args.do_train:
-        if args.load_checkpoint is not None:
+        if args.load_checkpoint and args.checkpoint is not None:
             model = module_class.load_from_checkpoint(
-                args.load_checkpoint,
+                args.checkpoint,
                 config=args
             )
         else:
@@ -55,16 +52,17 @@ def run(args):
 
     if args.do_eval:
         try:
-            assert args.load_checkpoint is not None
+            assert args.checkpoint is not None
         except AssertionError:
             util_logger.error('Checkpoint path is not provided for evaluation')
+
         if args.baseline:
             model = CausalLMModule(args)
-            trainer.test(model, ckpt_path=args.load_checkpoint)
+            trainer.test(model, ckpt_path=args.checkpoint)
         else:
             # train on 1 datapoint for gradient enabling in validation
             model = module_class.load_from_checkpoint(
-                args.load_checkpoint,
+                args.checkpoint,
                 config=args
             )
             trainer.fit(model)
