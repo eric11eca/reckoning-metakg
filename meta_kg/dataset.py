1a5523f6fe965ff526a1bf5a8e5981167d772c5e
Zeming Chen <zeming.chen@epfl.ch>
2023-09-24 01:09:47 +0200

    add seq2seq model (T5) data constructor

--- meta_kg/dataset.py
+++ meta_kg/dataset.py
@@ -8,6 +8,13 @@ from torch.utils.data import RandomSampler, SequentialSampler
 from .utils.py_io import read_jsonl
 
 
+def kg_span_reconstruction(text):
+    masked = text
+    for i, token in enumerate(text.split(" ")):
+        masked.replace(token, f"<extra_id_{i}>")
+    return masked
+
+
 class DataReader:
     """Custom dataset loader for QA problems."""
 
@@ -31,18 +38,17 @@ class DataReader:
         :param config: the configuration
         """
         total_data = read_jsonl(path)
-        all_facts = [fact for data in total_data for fact in data['facts']]
+        all_facts = [fact for data in total_data for fact in data["facts"]]
 
         total_qa_data = []
         for instance in total_data:
             qa_data = cls._read(instance, config)
             if config.random_facts:
-                num_facts = len(qa_data[0]['facts'])
-                qa_data[0]['facts'] = random.choices(all_facts, k=num_facts)
+                num_facts = len(qa_data[0]["facts"])
+                qa_data[0]["facts"] = random.choices(all_facts, k=num_facts)
             total_qa_data += qa_data
 
-        for data in total_qa_data[:2]:
-            pprint(data)
+        pprint(total_qa_data[0])
 
         return total_qa_data
 
@@ -58,11 +64,7 @@ class ProofWriterDataReader(DataReader):
         :param args: the configuration arguments
         :rtype instance: situation_modeling.readers.input_example.InputBase
         """
-        answer_map = {
-            "true": "yes",
-            "false": "no",
-            "unknown": "none"
-        }
+        answer_map = {"true": "yes", "false": "no", "unknown": "none"}
         guid = instance["guid"]
         question = instance["question"].replace(".", "")
         if "all_facts" in instance:
@@ -297,7 +299,7 @@ class MetaKnowledgeDataset(object):
         return self.dataloader
 
 
-class MetaDataLoader():
+class MetaDataLoader:
     def __init__(self, args, dataset, tokenizer, is_training):
         self.args = args
         self.task = args.dataset_type
@@ -313,11 +315,16 @@ class MetaDataLoader():
             sampler = SequentialSampler(dataset)
             batch_size = args.predict_batch_size
 
-        if args.baseline:
-            collate_fn = self.causal_lm_base_collator
+        if args.model_type == "t5":
+            if args.baseline:
+                collate_fn = self.text2text_baseline_collator
+            else:
+                collate_fn = self.text2text_collator
         else:
-            collate_fn = self.causal_lm_collator
-
+            if args.baseline:
+                collate_fn = self.causal_lm_base_collator
+            else:
+                collate_fn = self.causal_lm_collator
         self.dataloader = DataLoader(
             dataset,
             sampler=sampler,
@@ -340,20 +347,25 @@ class MetaDataLoader():
         ids1 = self.tokenizer(input_txt, return_tensors="pt")["input_ids"]
         ids2 = self.tokenizer(output_txt, return_tensors="pt")["input_ids"]
 
-        max_length = ids1.size(
-            1) + ids2.size(1) if max_length is None else max_length
+        max_length = ids1.size(1) + ids2.size(1) if max_length is None else max_length
         n_mask = max_length - ids1.size(1) - ids2.size(1)
         assert n_mask >= 0, (max_length, ids1.size(1), ids2.size(1))
         padding = torch.LongTensor(pad_id * n_mask).unsqueeze(0)
 
         input_ids = torch.cat((ids1, ids2, padding), dim=1)
         attention_mask = torch.LongTensor(
-            [1] * (ids1.size(1) + ids2.size(1)) + [0] * n_mask).unsqueeze(0)
+            [1] * (ids1.size(1) + ids2.size(1)) + [0] * n_mask
+        ).unsqueeze(0)
         token_type_ids = torch.LongTensor(
-            [0] * ids1.size(1) + [1] * ids2.size(1) + [0] * n_mask).unsqueeze(0)
+            [0] * ids1.size(1) + [1] * ids2.size(1) + [0] * n_mask
+        ).unsqueeze(0)
 
-        assert input_ids.size(1) == attention_mask.size(
-            1) == token_type_ids.size(1) == max_length
+        assert (
+            input_ids.size(1)
+            == attention_mask.size(1)
+            == token_type_ids.size(1)
+            == max_length
+        )
 
         return input_ids, attention_mask, token_type_ids
 
@@ -366,15 +378,16 @@ class MetaDataLoader():
         eos = self.tokenizer.eos_token
         bos = self.tokenizer.bos_token
 
-        facts_batch = ["\n".join([fact for fact in data['facts']]) for data in batch]
+        facts_batch = ["\n".join([fact for fact in data["facts"]]) for data in batch]
         questions = [f"{data['qa_pairs'][0][0]}" for data in batch]
-        answers = [data['qa_pairs'][0][1] for data in batch]
+        answers = [data["qa_pairs"][0][1] for data in batch]
 
         max_length = 0
         inputs = []
         for facts, question, answer in zip(facts_batch, questions, answers):
             fact_prefix = f"{bos}{facts}"
             input_txt = f"{fact_prefix}\n{question}"
+
             if self.args.no_facts:
                 input_txt = question
             output_txt = f"{answer}{eos}"
@@ -389,21 +402,21 @@ class MetaDataLoader():
             max_length = max(max_length, ids1.size(1) + ids2.size(1))
 
         input_ids_batch, attention_mask_batch, token_type_ids_batch = [], [], []
-        for (txt_in, txt_out) in inputs:
+        for txt_in, txt_out in inputs:
             input_ids, attention_mask, token_type_ids = self._tensorize(
-                txt_in, txt_out, max_length)
-
+                txt_in, txt_out, max_length
+            )
             input_ids_batch.append(input_ids)
             attention_mask_batch.append(attention_mask)
             token_type_ids_batch.append(token_type_ids)
 
         print_inputs = [txt_in for (txt_in, _) in inputs]
-        if len(batch[0]['qa_pairs'][0]) > 2:
-            print_outputs = [data['qa_pairs'][0][2] for data in batch]
+        if len(batch[0]["qa_pairs"][0]) > 2:
+            print_outputs = [data["qa_pairs"][0][2] for data in batch]
         else:
-            print_outputs = [data['qa_pairs'][0][1] for data in batch]
+            print_outputs = [data["qa_pairs"][0][1] for data in batch]
         print_out = {
-            "guid": [data['guid'] for data in batch],
+            "guid": [data["guid"] for data in batch],
             "prefix": [self.args.dataset for data in batch],
             "question": print_inputs,
             "answer": print_outputs,
@@ -435,14 +448,14 @@ class MetaDataLoader():
             train_attention_mask_batch = []
             train_token_type_ids_batch = []
             train_samples = []
-            for fact in qa_data['facts']:
-                fact_pair = fact.split(':')
+            for fact in qa_data["facts"]:
+                fact_pair = fact.split(":")
                 train_input_txt = f"{fact_pair[0].strip()}: "
                 train_output_txt = f"{fact_pair[1].strip()}"
-                ids1 = self.tokenizer(
-                    train_input_txt, return_tensors="pt")["input_ids"]
-                ids2 = self.tokenizer(
-                    train_output_txt, return_tensors="pt")["input_ids"]
+                ids1 = self.tokenizer(train_input_txt, return_tensors="pt")["input_ids"]
+                ids2 = self.tokenizer(train_output_txt, return_tensors="pt")[
+                    "input_ids"
+                ]
                 max_length = max(max_length, ids1.size(1) + ids2.size(1))
                 train_samples.append((train_input_txt, train_output_txt))
 
@@ -463,8 +476,7 @@ class MetaDataLoader():
             for qa_pair in qa_data["qa_pairs"]:
                 dev_input_txt = qa_pair[0]
                 dev_output_txt = f"{qa_pair[1]}{eos}"
-                dev_samples.append(
-                    (dev_input_txt, dev_output_txt.replace(eos, '')))
+                dev_samples.append((dev_input_txt, dev_output_txt.replace(eos, "")))
                 input_ids, attention_mask, token_type_ids = self._tensorize(
                     dev_input_txt, dev_output_txt)
                 dev_input_ids_batch.append(input_ids)
@@ -486,10 +498,8 @@ class MetaDataLoader():
             }
 
             if self.evaluate:
-                train_inputs = [
-                    fact[0] for fact in train_samples]
-                train_outputs = [
-                    fact[1] for fact in train_samples]
+                train_inputs = [fact[0] for fact in train_samples]
+                train_outputs = [fact[1] for fact in train_samples]
                 dev_inputs_eval = [sample[0] for sample in dev_samples]
                 dev_outputs = [sample[1] for sample in dev_samples]
 
@@ -504,5 +514,161 @@ class MetaDataLoader():
                     "fact": train_outputs,
                     "guid": [qa_data["guid"]]
                 }
+            batch_features.append(feature)
+        return batch_features
+    def text2text_baseline_collator(
+        self,
+        batch,
+    ):
+        """Batch collator for this custom class
+        :param batch: an incoming batch
+        :param tokenizer: the model tokenizer
+        :param args: the global configuration
+        """
+        facts_batch = ["\n".join([fact for fact in data["facts"]]) for data in batch]
+        questions = [f"{data['qa_pairs'][0][0]}" for data in batch]
+        answers = [data["qa_pairs"][0][1] for data in batch]
+
+        inputs, outputs = [], []
+        for facts, question, answer in zip(facts_batch, questions, answers):
+            inputs.append(f"{facts}\n{question}")
+            outputs.append(answer)
+
+        input_max_length = max([len(self.tokenizer.encode(i)) for i in inputs])
+        output_max_length = max([len(self.tokenizer.encode(o)) for o in outputs])
+
+        tokenized_inputs = self.tokenizer(
+            inputs,
+            max_length=input_max_length,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
+        )
+
+        tokenized_outputs = self.tokenizer(
+            outputs,
+            max_length=output_max_length,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
+        )
+
+        if len(batch[0]["qa_pairs"][0]) > 2:
+            print_outputs = [data["qa_pairs"][0][2] for data in batch]
+        else:
+            print_outputs = [data["qa_pairs"][0][1] for data in batch]
+
+        print_out = {
+            "guid": [data["guid"] for data in batch],
+            "prefix": [self.args.dataset for _ in batch],
+            "question": inputs,
+            "answer": print_outputs,
+        }
+
+        feature = {
+            "input_ids": tokenized_inputs["input_ids"],
+            "attention_mask": tokenized_inputs["attention_mask"],
+            "labels": tokenized_outputs["input_ids"],
+            "print_out": print_out,
+            "evaluate": self.evaluate,
+        }
+        return feature
+
+    def text2text_collator(
+        self,
+        batch,
+    ):
+        """Batch collator for this custom class
+        :param batch: an incoming batch
+        :param tokenizer: the model tokenizer
+        :param args: the global configuration
+        """
+        batch_features = []
+        for qa_data in batch:
+            train_input_txt_batch = []
+            train_output_txt_batch = []
+            for fact in qa_data["facts"]:
+                fact_pair = fact.split(":")
+                train_output_txt = fact_pair[1].strip()
+                masked = kg_span_reconstruction(train_output_txt)
+                train_input_txt = f"{fact_pair[0].strip()}: {masked}"
+                train_input_txt_batch.append(train_input_txt)
+                train_output_txt_batch.append(train_output_txt)
+
+            input_max_length = [
+                len(self.tokenizer.encode(e)) for e in train_input_txt_batch
+            ]
+            output_max_length = [
+                len(self.tokenizer.encode(e)) for e in train_output_txt_batch
+            ]
+
+            train_tokenized_input = self.tokenizer.batch_encode_plus(
+                train_input_txt_batch,
+                padding=True,
+                truncation=True,
+                return_tensors="pt",
+                max_length=max(input_max_length),
+            )
+            train_tokenized_output = self.tokenizer.batch_encode_plus(
+                train_output_txt_batch,
+                padding=True,
+                truncation=True,
+                return_tensors="pt",
+                max_length=max(output_max_length),
+            )
+
+            dev_input_txt_batch = []
+            dev_output_txt_batch = []
+            labels_batch = []
+            dev_samples = []
+            for qa_pair in qa_data["qa_pairs"]:
+                dev_input_txt_batch.append(qa_pair[0])
+                dev_output_txt_batch.append(qa_pair[1])
+                labels_batch.append(qa_pair[1])
+                dev_samples.append((qa_pair[0], qa_pair[1]))
+
+            input_max_length = [
+                len(self.tokenizer.encode(e)) for e in dev_input_txt_batch
+            ]
+            output_max_length = [
+                len(self.tokenizer.encode(e)) for e in dev_output_txt_batch
+            ]
+
+            dev_tokenized_input = self.tokenizer.batch_encode_plus(
+                dev_input_txt_batch,
+                padding=True,
+                truncation=True,
+                return_tensors="pt",
+                max_length=max(input_max_length),
+            )
+            dev_tokenized_output = self.tokenizer.batch_encode_plus(
+                dev_output_txt_batch,
+                padding=True,
+                truncation=True,
+                return_tensors="pt",
+                max_length=max(output_max_length),
+            )
+
+            feature = {
+                "input_ids": dev_tokenized_input["input_ids"],
+                "attention_mask": dev_tokenized_input["attention_mask"],
+                "labels": dev_tokenized_output["input_ids"],
+                "train_input_ids": train_tokenized_input["input_ids"],
+                "train_attention_mask": train_tokenized_input["attention_mask"],
+                "train_labels": train_tokenized_output["input_ids"],
+                "print_out": {"guid": [qa_data["guid"]]},
+                "evaluate": self.evaluate,
+            }
+
+            if self.evaluate:
+                dev_inputs_eval = [sample[0] for sample in dev_samples]
+                dev_outputs = [sample[1] for sample in dev_samples]
+                feature["print_out"].update(
+                    {
+                        "question": dev_inputs_eval,
+                        "answer": dev_outputs,
+                        "guid": [qa_data["guid"]],
+                    }
+                )
             batch_features.append(feature)
         return batch_features
